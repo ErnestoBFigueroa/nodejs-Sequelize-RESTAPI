146e0da09c4f9d3299c5de1c904ae024a6a8715f
ErnestoBFigueroa <Ingenes@INGENCOTIMO21.ingenes.local>
2023-12-18 00:58:27 -0600

    Sequelize

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
 import express from "express";
 import projectsRoutes from "./routes/projects.routes.js";
+import cors from "cors";
+import {
+  DB_DATABASE,
+  DB_HOST,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER,
+  FRONTEND_URL,
+  PORT,
+} from "./config.js";
 
 const app = express();
+const whiteList = [FRONTEND_URL];
+
+app.use(
+  cors({
+    origin: whiteList,
+  })
+);
 
 // middlewares
 app.use(express.json());
--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,20 @@
 import { Sequelize } from "sequelize";
+import {
+  DB_DATABASE,
+  DB_HOST,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER,
+  FRONTEND_URL,
+  PORT,
+} from "../config.js";
 
-export const sequelize = new Sequelize ('Adincoha', 'postgres', 'Carambola83$', {
-    host: 'localhost',
-    dialect: 'postgres'
-})
+export const sequelize = new Sequelize(
+  DB_DATABASE, //'Adincoha',
+  DB_USER, //"postgres",
+  DB_PASSWORD, //"Carambola83$",
+  {
+    host: DB_HOST, //"localhost",
+    dialect: "postgres",
+  }
+);
